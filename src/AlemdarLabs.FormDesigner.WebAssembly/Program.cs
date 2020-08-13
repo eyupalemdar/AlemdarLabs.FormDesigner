c8f1eb79a76e60045518f9e6a184650172343260
eyupalemdar <eyupalemdar@outlook.com>
2020-08-13 11:44:13 +0300

    WebAssembly is implemented

--- src/AlemdarLabs.FormDesigner.WebAssembly/Program.cs
+++ src/AlemdarLabs.FormDesigner.WebAssembly/Program.cs
@@ -1,14 +1,10 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
-namespace AlemdarLabs.FormDesigner.Client
+namespace AlemdarLabs.FormDesigner
 {
     public class Program
     {
@@ -17,7 +13,7 @@ namespace AlemdarLabs.FormDesigner.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
